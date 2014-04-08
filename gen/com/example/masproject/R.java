668c1ce4cd86b53ab32a9567014c014feaa73e26
jschrader54 <jschrader3@gatech.edu>
2014-04-07 22:58:25 -0400

    Misc fixes

--- gen/com/example/masproject/R.java
+++ gen/com/example/masproject/R.java
@@ -1092,7 +1092,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050048;
+        public static final int action_settings=0x7f05004c;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1108,6 +1108,8 @@ containing a value of this type.
         public static final int editText1=0x7f05003e;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
+        public static final int et_pw=0x7f05004b;
+        public static final int et_un=0x7f050049;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
@@ -1149,6 +1151,8 @@ containing a value of this type.
         public static final int textView1=0x7f050040;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int tv_pw=0x7f05004a;
+        public static final int tv_un=0x7f050048;
         public static final int txtName=0x7f050043;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
@@ -1210,7 +1214,8 @@ containing a value of this type.
         public static final int fragment_first_screen=0x7f03001b;
         public static final int list_group=0x7f03001c;
         public static final int list_item=0x7f03001d;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
+        public static final int login_screen=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
         public static final int first_screen=0x7f0c0000;
