2c0dc696ca68e41f53a9719aee9c244867fdda58
jschrader54 <jschrader3@gatech.edu>
2014-04-14 18:29:13 -0400

    Switch to JSON objects, View by Day Working Now
    Parsing/Mapping code I created was taking too long, using native
    libraries I just found instead. (what a womping waste of time)

--- src/com/example/masproject/ActivityScreen.java
+++ src/com/example/masproject/ActivityScreen.java
@@ -6,22 +6,17 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
@@ -35,24 +30,19 @@ import android.widget.Toast;
 public class ActivityScreen extends Activity {
 	TextView txtDate;
 	Calendar cal;
-	String dateTitle;
-	String dateUrl;
 	SimpleDateFormat titleFormat = new SimpleDateFormat("EEEE, MM/d/yy");	
 	SimpleDateFormat urlFormat = new SimpleDateFormat("y-MM-d");	 
+	String dateTitle;
+	String dateUrl;
 	String infoUrl = "http://dev.m.gatech.edu/developer/pconner3/widget/4261/c/api/events?date=";
 	String actUrl = "http://dev.m.gatech.edu/developer/pconner3/widget/4261/c/api/activities";
 	String jSessionid;
-	String msg;
-	List<Map<String, String>> eventList;
-	List<Map<String, String>> possList;
+    JSONArray jadd;
+    JSONArray jday;
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
     List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
-    
-    JSONArray jadd;
-    JSONArray jday;
-    JSONObject jobj;
+    HashMap<String, List<String>> listDataChild;    
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,8 +57,7 @@ public class ActivityScreen extends Activity {
         cal.setTime(new Date());         
         // Getting session cookie from login screen 
         Intent i = getIntent();
-        jSessionid = i.getStringExtra("sess");
-        Log.i("Activity Screen", jSessionid);        
+        jSessionid = i.getStringExtra("sess");     
         // Buttons
         Button btnPrev = (Button) findViewById(R.id.btnPrev);
         Button btnNext = (Button) findViewById(R.id.btnNext);
@@ -78,8 +67,8 @@ public class ActivityScreen extends Activity {
             	cal.add(Calendar.DATE, -1);  
             	dateTitle = titleFormat.format(cal.getTime());  
     	        txtDate.setText(dateTitle);
-            	dateUrl = urlFormat.format(cal.getTime());     
-            	getDateData();           
+            	dateUrl = urlFormat.format(cal.getTime());       
+            	updateListData();
             }
         });	 
         // Next day button
@@ -89,7 +78,7 @@ public class ActivityScreen extends Activity {
             	dateTitle = titleFormat.format(cal.getTime()); 
     	        txtDate.setText(dateTitle); 
             	dateUrl = urlFormat.format(cal.getTime());   
-            	getDateData();
+            	updateListData();
             }
         });	 
         // Get the listview
@@ -114,18 +103,18 @@ public class ActivityScreen extends Activity {
         expListView.setOnGroupExpandListener(new OnGroupExpandListener() {	 
             @Override
             public void onGroupExpand(int groupPosition) {
-                Toast.makeText(getApplicationContext(),
-                        listDataHeader.get(groupPosition) + " Expanded",
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(),
+//                        listDataHeader.get(groupPosition) + " Expanded",
+//                        Toast.LENGTH_SHORT).show();
             }
         });	 
         // Listview Group collasped listener
         expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {	 
             @Override
             public void onGroupCollapse(int groupPosition) {
-                Toast.makeText(getApplicationContext(),
-                        listDataHeader.get(groupPosition) + " Collapsed",
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(),
+//                        listDataHeader.get(groupPosition) + " Collapsed",
+//                        Toast.LENGTH_SHORT).show();
             }
         });	 
         // Listview on child click listener
@@ -140,52 +129,34 @@ public class ActivityScreen extends Activity {
                 return false;
             }
         });
+        // Expand Activity List by default
+        expListView.expandGroup(1);
     }
-    @SuppressWarnings("unchecked")
-	public void mapReply(String src) throws IOException {   
-    	//convert string reply to list of mapped event data
-    	eventList = new ArrayList<Map<String, String>>();
-    	String[] events = src.split("\\},\\{");
-    	for (int i = 0;i<events.length;i++) {
-    		if (i == 0) {
-    	    	events[i] = events[i].substring(1) + "}";    			
-    		} else if (i==events.length-1) {
-    			events[i] = "{" + events[i].substring(0,events[i].length()-1);    			
-    		} else {
-    			events[i] = "{" + events[i] + "}";    			
-    		}
-    		eventList.add((Map<String,String>) new ObjectMapper().readValue(events[i], new TypeReference<Map<String,String>>() {}));
-            System.out.println("Got " + eventList.get(i)); 
-    	}
-    } 
-	// Get data for specific date
-    void getDateData() {
-        HTTPInteraction httpobj= new HTTPInteraction();
-		try {
-			HttpGet request = new HttpGet(infoUrl + dateUrl);
-			request.setHeader("Cookie", "PHPSESSID=" + jSessionid);
-			DefaultHttpClient httpclient = new DefaultHttpClient();
-            try {
-				HttpResponse resp = httpclient.execute(request);
-				msg = httpobj.parseResponse(resp);
-		        mapReply(msg);
-				//jarr = new JSONArray(msg);
-			} catch (ClientProtocolException e) {
+    // Update data when day changes
+    private void updateListData() {  
+        expListView.collapseGroup(0); 	
+        expListView.collapseGroup(1);  
+    	int i = 0;
+        List<String> activitylist = new ArrayList<String>();
+    	getDateData();
+        for (i=0;i<jday.length();i++) {
+        	try {
+				activitylist.add(jday.getJSONObject(i).getString("Name"));
+			} catch (JSONException e) {
 				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}	
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			}
+        }
+        listDataChild.put(listDataHeader.get(1), activitylist); 
+        expListView.expandGroup(1); 	
     }
-    /*Preparing the list data*/
+    // List data init
     private void prepareListData() {
     	int i = 0;
-    	getDateData();
-    	getActData();
         listDataHeader = new ArrayList<String>();
         listDataChild = new HashMap<String, List<String>>();
+        // Getting data from REST
+    	getDateData();
+    	getActData();
         // Adding header data
         listDataHeader.add("Add New Activity");
         listDataHeader.add("Activity List"); 
@@ -197,15 +168,38 @@ public class ActivityScreen extends Activity {
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
-        	//newactivity.add(possList.get(i).get("Name"));
         }    
         List<String> activitylist = new ArrayList<String>();
-        for (i=0;i<eventList.size();i++) {
-        	activitylist.add(eventList.get(i).get("Name"));
+        for (i=0;i<jday.length();i++) {
+        	try {
+				activitylist.add(jday.getJSONObject(i).getString("Name"));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
         }
         // putting data in list
         listDataChild.put(listDataHeader.get(0), newactivity);
         listDataChild.put(listDataHeader.get(1), activitylist);
+    }
+    // Get data for specific date
+    void getDateData() {
+        HTTPInteraction httpobj= new HTTPInteraction();
+		try {
+			HttpGet request = new HttpGet(infoUrl + dateUrl);
+			request.setHeader("Cookie", "PHPSESSID=" + jSessionid);
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+            try {
+				HttpResponse resp = httpclient.execute(request);
+				String src = httpobj.parseResponse(resp);
+				jday = new JSONArray(src);
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 	// Get all possible activities
 	void getActData() {
@@ -217,20 +211,6 @@ public class ActivityScreen extends Activity {
             try {
 				HttpResponse resp = httpclient.execute(request);
 				String src = httpobj.parseResponse(resp);
-				//convert string reply to list of mapped event data
-//		    	possList = new ArrayList<Map<String, String>>();
-//		    	String[] events = src.split("\\},\\{");
-//		    	for (int i = 0;i<events.length;i++) {
-//		    		if (i == 0) {
-//		    	    	events[i] = events[i].substring(1) + "}";    			
-//		    		} else if (i==events.length-1) {
-//		    			events[i] = "{" + events[i].substring(0,events[i].length()-1);    			
-//		    		} else {
-//		    			events[i] = "{" + events[i] + "}";    			
-//		    		}
-//		    		possList.add((Map<String,String>) new ObjectMapper().readValue(events[i], new TypeReference<Map<String,String>>() {}));
-//		            System.out.println("Got " + possList.get(i)); 
-//		    	}
 				jadd = new JSONArray(src);				
 			} catch (ClientProtocolException e) {
 				e.printStackTrace();
